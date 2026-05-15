4948b01f04474dfa8cf0b675d63012d8d311e4d2
samchen <wmydz1@gmail.com>
2026-05-14 19:46:24 -0700

    fix script

--- build-docker.sh
+++ build-docker.sh
@@ -7,7 +7,7 @@ docker push bosbase/bosbase:ve1
 
 
 
-docker build -t bosbase:0.0.1 -f docker/dockerfile .
+docker build -t bosbase:0.0.1 -f docker/Dockerfile .
 docker tag bosbase:0.0.1 bosbase/bosbase:0.0.1
 docker tag bosbase:0.0.1 bosbase/bosbase:latest
 docker push bosbase/bosbase:0.0.1
